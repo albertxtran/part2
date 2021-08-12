39cfa88c7d393dbcf779c7edadf343bdc57abd22
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-08-12 14:29:13 -0700

    refactoring modules

--- course-examples/src/App.js
+++ course-examples/src/App.js
@@ -1,16 +1,16 @@
-function App(props) {
-  const { notes } = props;
+import Note from "./components/Note";
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note, i) => (
-          <li key={i}>{note.content}</li>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default App;
