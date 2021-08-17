d538a62132b300eae352e10d89bf5713a53ae221
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-08-17 16:58:13 -0700

    2.11 The Phonebook Step 6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -6,6 +7,12 @@ import Persons from "./components/Persons";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newFilter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const handleFilterChange = (value) => {
     setFilter(value);
