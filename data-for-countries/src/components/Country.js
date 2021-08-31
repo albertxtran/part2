84ecfeaee339ed0615007660f63e91f3d41e804a
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-08-31 10:53:21 -0700

    2.14 data for countries, step 3
    no api key version

--- data-for-countries/src/components/Country.js
+++ data-for-countries/src/components/Country.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Country = ({ country }) => {
-  console.log("flag: ", country.flag);
+  const [weather, setWeather] = useState();
+  const lat = country.latlng[0];
+  const lng = country.latlng[1];
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://fcc-weather-api.glitch.me/api/current?lat=" +
+          lat +
+          "&lon=" +
+          lng
+      )
+      .then((response) => {
+        console.log("response: ", response);
+        setWeather(response.data);
+      });
+  }, []);
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -11,7 +31,15 @@ const Country = ({ country }) => {
           <li key={data.name}>{data.name}</li>
         ))}
       </ul>
-      <img src={country.flag} alt="new" />
+      <img src={country.flag} alt="new" style={{ width: 150, height: 150 }} />
+      <h2>Weather in {country.name}</h2>
+      <div>Temperature: {weather.main.temp}</div>
+      <img
+        src={weather.weather[0].icon}
+        alt="new"
+        style={{ width: 100, height: 100 }}
+      />
+      <div>Wind: {weather.wind.speed} mph</div>
     </div>
   );
 };
