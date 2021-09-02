1dbcb717cae507bb15155abed4bb5e1945d7f4f7
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-09-02 15:04:58 -0700

    Promises and Errors

--- course-examples/src/App.js
+++ course-examples/src/App.js
@@ -36,9 +36,15 @@ const App = () => {
   const toggleImportanceOf = (id) => {
     const note = notes.find((note) => note.id === id);
     const changedNote = { ...note, important: !note.important };
-    noteService.update(id, changedNote).then((returnedNote) => {
-      setNotes(notes.find((note) => (note.id === id ? returnedNote : note)));
-    });
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.find((note) => (note.id === id ? returnedNote : note)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from the server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   return (
