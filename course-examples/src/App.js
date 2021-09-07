8ce25eed5a3f16a9460705582e79b2be03677807
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-09-06 22:42:46 -0700

    Inline styles

--- course-examples/src/App.js
+++ course-examples/src/App.js
@@ -1,47 +1,43 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("some error happened...");
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    noteService.getAll().then((initialNotes) => setNotes(initialNotes));
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
   }, []);
 
   const addNote = (event) => {
     event.preventDefault();
-    const noteObj = {
+    const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
     };
-    noteService.create(noteObj).then((returnedNote) => {
+
+    noteService.create(noteObject).then((returnedNote) => {
       setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value);
-    setNewNote(event.target.value);
-  };
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
-
   const toggleImportanceOf = (id) => {
-    const note = notes.find((note) => note.id === id);
+    const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
+
     noteService
       .update(id, changedNote)
       .then((returnedNote) => {
-        setNotes(notes.find((note) => (note.id === id ? returnedNote : note)));
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
         setErrorMessage(
@@ -50,9 +46,15 @@ const App = () => {
         setTimeout(() => {
           setErrorMessage(null);
         }, 5000);
-        setNotes(notes.filter((n) => n.id !== id));
       });
   };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
@@ -76,6 +78,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
